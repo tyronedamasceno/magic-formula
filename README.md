9677ebc8584f7c51dd55baf06e15c2f1135bda2a
Tyrone Damasceno <tyrone.damasceno@buser.com.br>
2021-04-06 12:11:18 -0300

    Working

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # magic-formula
 Uma forma de aplica a "fórmula mágica" de greenblat em ações da bolsa brasileira
+
+## Passo a passo
+
+1. Acesse https://statusinvest.com.br/acoes/busca-avancada, faça a busca sem nenhum filtro e faça o download do arquivo csv
+
+2. Salve o arquivo na pasta do projeto com o nome **acoes-list.csv**
+
+3. Rode o comando `python magic_formula.py`
+
+4. A lista das 20 ações melhores rankeadas será printada no console e escrita no arquivo **resultado.txt**
---
+++ magic_formula.py
@@ -0,0 +1,65 @@
+import csv
+from collections import defaultdict
+from typing import List
+
+
+class Acao:
+    def __init__(self, ticker: str, ebit: str, pl: str, roe: str):
+        self.ticker = ticker
+        self.ebit = self.parse_float(ebit)
+        self.pl = self.parse_float(pl)
+        self.roe = self.parse_float(roe)
+
+    def parse_float(self, value: str) -> float:
+        if value:
+            return float(value.replace('.', '').replace(',', '.'))
+
+class MagicFormula:
+    def __init__(self):
+        self.acoes_rank = defaultdict(int)
+
+    def run(self, acoes: List[Acao]):
+        acoes = self.filter_positive_ebit(acoes)
+        acoes = self.filter_pl_greater_than_5(acoes)
+        acoes_by_pl = self.order_by_pl(acoes)
+        acoes_by_roe = self.order_by_roe(acoes)
+        self.add_to_rank(acoes_by_pl)
+        self.add_to_rank(acoes_by_roe)
+        best_acoes = self.get_best_acoes()
+        self.print_and_write_file(best_acoes)
+
+    def filter_positive_ebit(self, acoes_list):
+        return [acao for acao in acoes_list if acao.ebit and acao.ebit > 0]
+
+    def filter_pl_greater_than_5(self, acoes_list):
+        return [acao for acao in acoes_list if acao.pl and acao.pl > 5]
+
+    def order_by_pl(self, acoes_list):
+        return [acao.ticker for acao in sorted(acoes_list, key=lambda acao: acao.pl)]
+    
+    def order_by_roe(self, acoes_list):
+        return [acao.ticker for acao in sorted(acoes_list, key=lambda acao: -acao.roe)]
+
+    def add_to_rank(self, acoes_list):
+        for idx, ticker in enumerate(acoes_list):
+            self.acoes_rank[ticker] += idx
+
+    def get_best_acoes(self):
+        return sorted(self.acoes_rank.items(), key=lambda par: par[1])[:25]
+
+    def print_and_write_file(self, acoes_list):
+        print(acoes_list)
+        with open('resultado.txt', 'w') as f:
+            for ticker, value in acoes_list:
+                f.write(ticker + '\n')
+
+
+f = open('acoes-list.csv')
+
+reader = csv.DictReader(f, delimiter=';')
+acoes = [
+    Acao(row['TICKER'], row['MARGEM EBIT'], row['P/L'], row['ROE'])
+    for row in reader
+]
+
+MagicFormula().run(acoes)
